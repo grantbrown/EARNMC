31c370a436a07a19a1734a08d080ff80180c27d9
Grant Brown <grant.brown73@gmail.com>
2015-01-26 07:19:29 -0600

    Typo

--- scripts/prediction/estimatePredictionPerformance.R
+++ scripts/prediction/estimatePredictionPerformance.R
@@ -2,5 +2,5 @@ library(EARNMC)
 
 for (tpts in c(20,15,10,5)){
     result = WestAfricaAnalysisScript(3, throwAwayTpts=tpts)
-    save("result", file=paste("./pred_performance_dropping_", result, ".Rda.bz2", sep =""), compress="bzip2")
+    save("result", file=paste("./pred_performance_dropping_", tpts, ".Rda.bz2", sep =""), compress="bzip2")
 }
