b6f0ef1b26accfeb78b1831eaa45fe0086f969ba
Grant Brown <grant.brown73@gmail.com>
2015-01-29 20:35:23 -0600

    save raw prediction information

--- EARNMC/R/WA_AnalysisFunction.R
+++ EARNMC/R/WA_AnalysisFunction.R
@@ -706,7 +706,8 @@ predlist=list(Guinea.mean=Guinea.mean,
               SierraLeone.LB=SierraLeone.LB,
               Guinea.UB=Guinea.UB,
               Liberia.UB=Liberia.UB,
-              SierraLeone.UB=SierraLeone.UB)
+              SierraLeone.UB=SierraLeone.UB,
+              rawPreds=preds)
 
 save("chains", "predlist", file="./chainOutput.Robj")
 
