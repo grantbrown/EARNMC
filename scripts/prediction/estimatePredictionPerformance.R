4b46c615f9fb749cc4a6bb5c7698f75eb03c3627
Grant Brown <grant.brown73@gmail.com>
2015-01-31 13:10:38 -0600

    parallelize script

--- scripts/prediction/estimatePredictionPerformance.R
+++ scripts/prediction/estimatePredictionPerformance.R
@@ -1,6 +1,17 @@
 library(EARNMC)
+library(parallel)
+# cluster of size 2, each cluster makes cluster of size 3, maxes out 8 cores
+cl = makeCluster(2)
 
-for (tpts in c(20,15,10,5)){
-    result = WestAfricaAnalysisScript(3, throwAwayTpts=tpts)
-    save("result", file=paste("./pred_performance_dropping_", tpts, ".Rda.bz2", sep =""), compress="bzip2")
+runFunc = function(params){
+    DF = params[[1]]
+    tpts = params[[2]]
+    result = WestAfricaAnalysisScript(DF, throwAwayTpts=tpts)
+    save("result", file=paste("./DF",DF "/pred_performance_dropping_", tpts, ".Rda.bz2", sep =""), compress="bzip2")
 }
+
+DFs = c(0,1,2,3)
+tpts = c(20,15,10,5)
+params = expand.grid(DFs, tpts)
+paramsList = lapply(1:nrow(params), function(x){params[x,]})
+parLapply(cl, paramsList, runFunc)
