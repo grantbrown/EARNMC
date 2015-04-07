08aeec1d047a282a6dd49625ce377e0138b37bc7
Grant Brown <grant.brown73@gmail.com>
2015-04-06 20:03:19 -0500

    Add a combined plot

--- EARNMC/R/Kikwit_Graphs.R
+++ EARNMC/R/Kikwit_Graphs.R
@@ -139,9 +139,77 @@ makeKikwitGraphPDFs = function(fullSpecResultsFilename=NA,
         abline(h = 1.0, col = "blue", lwd = ThresholdWidth, lty = 2)
         lines(1:(length(r02$mean) - 1), r02$LB[1:(length(r02$LB)-1)], lty = 2, lwd = CI_width)
         lines(1:(length(r02$mean) - 1), r02$UB[1:(length(r02$UB)-1)], lty=2,lwd = CI_width)
-
-
     dev.off()
+
+    pdf(file="EA_R0_Comparison_Combined.pdf", width=12, height=6)
+    layout(matrix(c(1,2,3,
+                    4,2,5), nrow = 2, ncol = 3, byrow = TRUE))
+    
+    # Plot 1
+    r01 = KikwitUnderspecResults$R0Estimates[[1]][[1]]
+    r02 = KikwitUnderspecResults$R0Estimates[[1]][[2]]
+
+    min.r0 = min(min(KikwitUnderspecResults$R0Estimates[[1]][[1]]$LB), min(KikwitUnderspecResults$R0Estimates[[1]][[2]]$LB))
+    max.r0 = max(max(KikwitUnderspecResults$R0Estimates[[1]][[1]]$UB), max(KikwitUnderspecResults$R0Estimates[[1]][[2]]$UB) + 0.1)
+    r0.ylim = c(min.r0, max.r0)
+
+    plot(r01$mean[1:(length(r01$mean) - 1)], ylim = r0.ylim, main = "Traditional R0: Underspecified Intensity\n Posterior Mode and 90% CI",
+         xlab = "Day", ylab = "Reproductive Number", type = "l", lwd = MainLineWidth)
+    abline(h = seq(0,5,0.2), lty = 3,col="lightgrey")
+    abline(h = 1.0, col = "blue", lwd = ThresholdWidth, lty = 2)
+    lines(1:(length(r01$mean) - 1), r01$LB[1:(length(r01$LB)-1)], lty = 2, lwd = CI_width)
+    lines(1:(length(r01$mean) - 1), r01$UB[1:(length(r01$UB)-1)], lty = 2, lwd = CI_width)
+
+    # Plot 2
+    barplot(t(KikwitUnderspecResults$simResults$I_star), main = "New Cases", xlab = "Day", ylab = "Cases")
+    axis(side = 1, at = seq(0, (length(r01$mean)), 50))
+
+    # Plot 3
+    r01 = KikwitFullSpecResults$R0Estimates[[1]][[1]]
+    r02 = KikwitFullSpecResults$R0Estimates[[1]][[2]]
+
+    min.r0 = min(min(KikwitFullSpecResults$R0Estimates[[1]][[1]]$LB), min(KikwitFullSpecResults$R0Estimates[[1]][[2]]$LB))
+    max.r0 = max(max(KikwitFullSpecResults$R0Estimates[[1]][[1]]$UB), max(KikwitFullSpecResults$R0Estimates[[1]][[2]]$UB) + 0.1)
+    r0.ylim = c(min.r0, max.r0)
+
+    plot(r01$mean[1:(length(r01$mean) - 1)], ylim = r0.ylim, main = "Traditional R0\n Posterior Mode and 90% CI",
+         xlab = "Day", ylab = "Reproductive Number", type = "l", lwd = MainLineWidth)
+    abline(h = seq(0,5,0.2), lty = 3,col="lightgrey")
+    abline(h = 1.0, col = "blue", lwd = ThresholdWidth, lty = 2)
+    lines(1:(length(r01$mean) - 1), r01$LB[1:(length(r01$LB)-1)], lty = 2, lwd = CI_width)
+    lines(1:(length(r01$mean) - 1), r01$UB[1:(length(r01$UB)-1)], lty = 2, lwd = CI_width)
+
+    # Plot 4
+    r01 = KikwitUnderspecResults$R0Estimates[[1]][[1]]
+    r02 = KikwitUnderspecResults$R0Estimates[[1]][[2]]
+
+    min.r0 = min(min(KikwitUnderspecResults$R0Estimates[[1]][[1]]$LB), min(KikwitUnderspecResults$R0Estimates[[1]][[2]]$LB))
+    max.r0 = max(max(KikwitUnderspecResults$R0Estimates[[1]][[1]]$UB), max(KikwitUnderspecResults$R0Estimates[[1]][[2]]$UB) + 0.1)
+    r0.ylim = c(min.r0, max.r0)
+
+    plot(r02$mean[1:(length(r02$mean) - 1)], ylim = r0.ylim, main = "EA-R: Underspecified Intensity\n Posterior Mode and 90% CI",
+         xlab = "Day", ylab = "Reproductive Number", type = "l", lwd = MainLineWidth)
+    abline(h = seq(0,5,0.2), lty = 3,col="lightgrey")
+    abline(h = 1.0, col = "blue", lwd = ThresholdWidth, lty = 2)
+    lines(1:(length(r02$mean) - 1), r02$LB[1:(length(r02$LB)-1)], lty = 2, lwd = CI_width)
+    lines(1:(length(r02$mean) - 1), r02$UB[1:(length(r02$UB)-1)], lty = 2, lwd = CI_width)
+
+    # Plot 5
+    r01 = KikwitFullSpecResults$R0Estimates[[1]][[1]]
+    r02 = KikwitFullSpecResults$R0Estimates[[1]][[2]]
+
+    min.r0 = min(min(KikwitFullSpecResults$R0Estimates[[1]][[1]]$LB), min(KikwitFullSpecResults$R0Estimates[[1]][[2]]$LB))
+    max.r0 = max(max(KikwitFullSpecResults$R0Estimates[[1]][[1]]$UB), max(KikwitFullSpecResults$R0Estimates[[1]][[2]]$UB) + 0.1)
+    r0.ylim = c(min.r0, max.r0)
+
+    plot(r02$mean[1:(length(r02$mean) - 1)], ylim = r0.ylim, main = "EA-R\n Posterior Mode and 90% CI",
+         xlab = "Day", ylab = "Reproductive Number", type = "l", lwd = MainLineWidth)
+    abline(h = seq(0,5,0.2), lty = 3,col="lightgrey")
+    abline(h = 1.0, col = "blue", lwd = ThresholdWidth, lty = 2)
+    lines(1:(length(r02$mean) - 1), r02$LB[1:(length(r02$LB)-1)], lty = 2, lwd = CI_width)
+    lines(1:(length(r02$mean) - 1), r02$UB[1:(length(r02$UB)-1)], lty=2,lwd = CI_width)
+    dev.off()
+
 }
 
 
